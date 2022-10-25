a42325c15db1c0211824c46063938d1bae71f7a3
Waleed1600 <32969266+Waleed1600@users.noreply.github.com>
2022-10-25 18:11:47 +0300

    Add files via upload

---
+++ H3.py
@@ -0,0 +1,31 @@
+
+contactTable = [{'name': 'Ahmed', 'phone': '0551112222'}, {'name': 'Saad', 'phone': '0551113333'},
+
+                {'name': 'Sultan', 'phone': '0551114444'}, {'name': 'Morad', 'phone': '0551115555'}, {'name': 'Abdullah', 'phone': '0551116666'}]
+
+
+def phonebook():
+
+
+ user_number = input("Enter the phone number:-> ")
+
+
+ if len(user_number) != 10 or not user_number.isdigit():
+
+    return("Number is invalid")
+
+
+ for contact in contactTable:
+
+    if contact['phone'] == user_number:
+
+        return contact['name']
+
+
+
+ return("Sorry the number is not found")
+
+
+
+
+print(phonebook())
